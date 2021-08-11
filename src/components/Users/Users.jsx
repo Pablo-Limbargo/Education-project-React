3beaebed652f2f23d3a72ea3e177862515905a20
Pablo <schizart.ua@gmail.com>
2021-08-12 00:29:56 +0300

    52-53L - Side Effect, extends React.Component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,47 +1,31 @@
 import styles from "./users.module.css"
 import axios from "axios";
 import userPhoto from '../../assets/images/ava-no-image.png';
+import React from "react";
 
-let Users = (props) => {
+class Users extends React.Component {
 
-    if (props.users.length === 0) {
+    constructor(props) {
+        super(props);
 
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-            props.setUsers(response.data.items)
+            this.props.setUsers(response.data.items)
         });
-
-        // props.setUsers([
-        //     {
-        //         id: 1,
-        //         photoUrl: 'https://schoolsw3.com/tryit/avatar.png',
-        //         followed: false,
-        //         fullName: 'Oleg',
-        //         status: 'I want to eat',
-        //         location: {country: 'Ukraine', city: 'Odesa'}
-        //     },
-        //     {
-        //         id: 2,
-        //         photoUrl: 'https://html5css.ru/howto/img_avatar2.png',
-        //         followed: true,
-        //         fullName: 'Alla',
-        //         status: 'Teaching is my passion',
-        //         location: {country: 'Ukraine', city: 'Korosten'}
-        //     },
-        //     {
-        //         id: 3,
-        //         photoUrl: 'https://schoolsw3.com/tryit/avatar.png',
-        //         followed: false,
-        //         fullName: 'Alex',
-        //         status: 'Like to read books',
-        //         location: {country: 'United Kingdom', city: 'London'}
-        //     }
-        // ])
     }
 
+    // getUsers = () => {
+    //     if (this.props.users.length === 0) {
+    //
+    //         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+    //             this.props.setUsers(response.data.items)
+    //         });
+    //     }
+    // }
 
-    return <div className={styles.wrapper}>
-        {
-            props.users.map((u) => <div key={u.id}>
+    render() {
+        return <div className={styles.wrapper}>
+            {
+                this.props.users.map((u) => <div key={u.id}>
                 <span>
                     <div className={styles.userPhoto}>
                         <img alt='avatar-photo' src={u.photos.small != null ? u.photos.small : userPhoto}/>
@@ -49,14 +33,14 @@ let Users = (props) => {
                     <div>
                         {u.followed
                             ? <button onClick={() => {
-                                props.unfollow(u.id)
+                                this.props.unfollow(u.id)
                             }}>Unfollow</button>
                             : <button onClick={() => {
-                                props.follow(u.id)
+                                this.props.follow(u.id)
                             }}>Follow</button>}
                     </div>
                 </span>
-                <span>
+                    <span>
                     <span>
                         <div>{u.name}</div>
                         <div>{u.status}</div>
@@ -66,9 +50,11 @@ let Users = (props) => {
                         <div>{"u.location.city"}</div>
                     </span>
                 </span>
-            </div>)
-        }
-    </div>
+                </div>)
+            }
+        </div>
+
+    }
 }
 
 export default Users;
