f512bdbbc135c2962119f3732c8bf2e53bb48767
Pablo <schizart.ua@gmail.com>
2021-09-07 00:11:27 +0300

    71L - setState, local state

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import s from './ProfileInfo.module.css';
 import Preloader from "../../common/Preloader/Preloader";
 import userPhoto from "../../../assets/images/ava-no-image.png";
+import ProfileStatus from "./ProfileStatus";
 
 const ProfileInfo = (props) => {
     if (!props.profile) {
@@ -9,12 +10,13 @@ const ProfileInfo = (props) => {
     return (
         <div>
             <div>
-                <img alt='profile-img'
-                     src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg'/>
+                {/*<img alt='profile-img'*/}
+                {/*     src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg'/>*/}
             </div>
             <div className={s.descriptionBlock}>
                 <img alt='photo_large' src={props.profile.photos.large != null ? props.profile.photos.large : userPhoto}/>
                 <h3>Full Name: {props.profile.fullName}</h3>
+                <ProfileStatus status={'Hello!'}/>
                 <h3>About me: {props.profile.aboutMe}</h3>
                 <div>VK: <a href='https://vk.com/dimych'>{props.profile.contacts.vk}</a></div>
                 <div>twitter: <a href='https://twitter.com/@sdf'>{props.profile.contacts.twitter}</a></div>
