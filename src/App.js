f3326ddcfa649c52a925128f7caec3f13ac47a37
Pablo <schizart.ua@gmail.com>
2021-10-11 00:11:33 +0300

    99L - try-catch, router switch, classnames

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Navbar from "./components/Navbar/Navbar";
-import {HashRouter, Route, withRouter} from "react-router-dom"
+import {BrowserRouter, Route, withRouter, Switch, Redirect} from "react-router-dom"
 import Music from "./components/Music/Music";
 import News from "./components/News/News";
 import Settings from "./components/Settings/Settings";
@@ -20,9 +20,18 @@ const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileCo
 
 class App extends Component {
 
+    catchAllHandledErrors = (promiseRejectionEvent) => {
+        alert('Some error occurred');
+        //console.error(promiseRejectionEvent);
+    };
+
     componentDidMount() {
         this.props.initializeApp();
-    }
+        window.addEventListener('unhandledrejection', this.catchAllHandledErrors);
+    };
+    componentWillUnmount() {
+        window.removeEventListener('unhandledrejection', this.catchAllHandledErrors);
+    };
 
     render() {
 
@@ -37,39 +46,50 @@ class App extends Component {
                     // state={props.state}
                 />
                 <div className='app-wrapper-content'>
-                    <Route path='/dialogs' render={withSuspense(DialogsContainer)}/>
-                    <Route path='/profile/:userId?' render={withSuspense(ProfileContainer)}/>
-                    <Route path='/users' render={() => <UsersContainer/>}/>
-                    <Route path='/music' render={() => <Music/>}/>
-                    <Route path='/news' render={() => <News/>}/>
-                    <Route path='/settings' render={() => <Settings/>}/>
-                    <Route path='/login' render={() => <Login/>}/>
+                    <Switch>
+                        <Route exact path='/' render={ () => <Redirect to={'/profile'}/>}/>
+                        <Route path='/dialogs' render={withSuspense(DialogsContainer)}/>
+                        <Route path='/profile/:userId?' render={withSuspense(ProfileContainer)}/>
+                        <Route path='/users' render={() => <UsersContainer/>}/>
+                        <Route path='/music' render={() => <Music/>}/>
+                        <Route path='/news' render={() => <News/>}/>
+                        <Route path='/settings' render={() => <Settings/>}/>
+                        <Route path='/login' render={() => <Login/>}/>
+                        <Route path='*' render={() => <div>404 NOT FOUND</div>}/>
+                    </Switch>
                 </div>
             </div>
-        );
+    );
     }
-}
+    }
 
-const mapStateToProps = (state) => ({
-    initialized: state.app.initialized
-})
+    const mapStateToProps = (state) => (
+        {
+            initialized: state.app.initialized
+        }
+    )
 
-let AppContainer = compose(
+    let AppContainer = compose(
     withRouter,
-    connect(mapStateToProps, {initializeApp}))(App);
+    connect(mapStateToProps,
+        {
+            initializeApp
+        }
+    ))(App);
 
-const SocNetApp = (props) => {
-    return <HashRouter
-        //basename={process.env.PUBLIC_URL}
-    >
-        <Provider store={store}>
-            <AppContainer/>
-        </Provider>
-    </HashRouter>
-}
+    const SocNetApp = (props) =>
+        {
+            return <BrowserRouter
+                //basename={process.env.PUBLIC_URL}
+            >
+                <Provider store={store}>
+                    <AppContainer/>
+                </Provider>
+            </BrowserRouter>
+        }
 
-export default SocNetApp;
+    export default SocNetApp;
 
-// 19L - 'react-router-dom -save' - установка роутинга. Пишем роуты, обрамляем в браузер роутер
-// 26-27L - меняем component на render, чтобы можно было прокинуть пропсы
-// 30L - оборачиваем браузер роутер арр в файле индекс, тут убираем
+    // 19L - 'react-router-dom -save' - установка роутинга. Пишем роуты, обрамляем в браузер роутер
+    // 26-27L - меняем component на render, чтобы можно было прокинуть пропсы
+    // 30L - оборачиваем браузер роутер арр в файле индекс, тут убираем
